d9c3d68350254ba694d0777404b91995ce4ccaf0
jones58 <108466184+jones58@users.noreply.github.com>
2024-08-21 13:22:32 +0100

    setting up storybook

---
+++ storybook/.storybook/main.js
@@ -0,0 +1,19 @@
+/** @type { import('@storybook/react-vite').StorybookConfig } */
+const config = {
+  stories: ["../**/*.stories.@(js|jsx)"],
+  staticDirs: ["../public"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+};
+export default config;
---
+++ storybook/.storybook/preview.js
@@ -0,0 +1,23 @@
+import "../src/index.css";
+
+// Registers the msw addon
+import { initialize, mswLoader } from "msw-storybook-addon";
+
+// Initialize MSW
+initialize();
+
+/** @type { import('@storybook/react').Preview } */
+const preview = {
+  parameters: {
+    actions: { argTypesRegex: "^on[A-Z].*" },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+  loaders: [mswLoader],
+};
+
+export default preview;
