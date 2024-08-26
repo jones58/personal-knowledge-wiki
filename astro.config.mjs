57345a69dd07a60a867e457a105e4a4d8760bede
jones58 <108466184+jones58@users.noreply.github.com>
2024-08-26 16:54:53 +0100

    configuring site including edit links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,36 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 import sitemap from "@astrojs/sitemap";
-import starlightThemeRapide from 'starlight-theme-rapide'
+import starlightThemeRapide from "starlight-theme-rapide";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://knowledgewiki.jackkershaw.net",
-  integrations: [starlight({
-    plugins: [starlightThemeRapide()],
-    title: "My Knowledge Wiki",
-    social: {
-      github: "https://github.com/jones58/personal-knowledge-wiki"
-    },
-    sidebar: [{
-      label: "Reference",
-      autogenerate: {
-        directory: "reference"
-      }
-    }]
-  }), sitemap()]
+  description:
+    "This is my personal wiki, with all the links and notes that I find useful.",
+  integrations: [
+    starlight({
+      plugins: [starlightThemeRapide()],
+      logo: {
+        src: "/public/favicon.svg",
+      },
+      title: "My Knowledge Wiki",
+      social: {
+        github: "https://github.com/jones58/personal-knowledge-wiki",
+      },
+      editLink: {
+        baseUrl:
+          "https://github.com/jones58/personal-knowledge-wiki/edit/main/",
+      },
+      sidebar: [
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+    }),
+    sitemap(),
+  ],
 });
