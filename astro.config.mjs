25f0de75ad01901ed5e08b150bffbd011f7258f0
jones58 <108466184+jones58@users.noreply.github.com>
2024-08-26 15:54:04 +0100

    add tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-
 import sitemap from "@astrojs/sitemap";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://knowledgewiki.jackkershaw.net",
   integrations: [starlight({
     title: "My Knowledge Wiki",
     social: {
-      github: "https://github.com/withastro/starlight"
+      github: "https://github.com/jones58/personal-knowledge-wiki"
     },
     sidebar: [{
       label: "Reference",
@@ -16,5 +18,8 @@ export default defineConfig({
         directory: "reference"
       }
     }]
-  }), sitemap()]
+  }), sitemap(), tailwind({
+    // Disable the default base styles:
+    applyBaseStyles: false,
+  })]
 });
