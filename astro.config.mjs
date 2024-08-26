f3029bc72cff82cdd6310826567c988d2f57c768
jones58 <108466184+jones58@users.noreply.github.com>
2024-08-26 16:10:26 +0100

    add starlight theme rapide

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
-
 import sitemap from "@astrojs/sitemap";
+import starlightThemeRapide from 'starlight-theme-rapide'
+
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [starlight({
+    plugins: [starlightThemeRapide()],
     title: "My Knowledge Wiki",
     social: {
-      github: "https://github.com/withastro/starlight"
+      github: "https://github.com/jones58/personal-knowledge-wiki"
     },
     sidebar: [{
       label: "Reference",
