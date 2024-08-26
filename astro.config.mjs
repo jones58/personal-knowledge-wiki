3275a192bae00b6aa0002f9fa7d18c273f382d3a
jones58 <108466184+jones58@users.noreply.github.com>
2024-08-26 15:30:26 +0100

    added astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 import { defineConfig } from "astro/config";
 import starlight from "@astrojs/starlight";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "My Knowledge Wiki",
-      social: {
-        github: "https://github.com/withastro/starlight",
-      },
-      sidebar: [
-        {
-          label: "Reference",
-          autogenerate: { directory: "reference" },
-        },
-      ],
-    }),
-  ],
+  integrations: [starlight({
+    title: "My Knowledge Wiki",
+    social: {
+      github: "https://github.com/withastro/starlight"
+    },
+    sidebar: [{
+      label: "Reference",
+      autogenerate: {
+        directory: "reference"
+      }
+    }]
+  }), sitemap()]
 });
